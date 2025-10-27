ea665f92b68e00fa2f6fe82a5ea3365db3863b8e
Nnaemekaezeah <nnaemekahyginus1@gmail.com>
2025-10-27 19:48:01 +0100

    Add utility functions and refactor main
    Refactor main function and add multiple utility functions.

--- Practical-Task/Chapter1.hs
+++ Practical-Task/Chapter1.hs
@@ -1,8 +1,36 @@
 add :: Float -> Float -> Float
 add x y = x + y
 
-main :: 10 ()
+main :: IO ()
 main = do
  print $ add 5.1 2.1
-
 --Answer = 7.2
+double :: Int -> Int
+double x = x * 2
+
+main :: IO ()
+main = do
+ print (double 3)
+--Answer = 6
+increment :: Int -> Int
+increment x = x + 1
+
+main :: IO ()
+main = do
+ print (increment 5)
+ --Answer = 6
+ doubleThenIncrement :: Int -> Int
+ doubleThenIncrement x = x * 2 + 1
+
+ main :: IO ()
+ main = do
+  print (doubleThenIncrement 3)
+ --Answer = 7
+ circleArea :: Float -> Float
+ circleArea r = pi * r^2
+
+ main :: IO ()
+ main = do
+  print (circleArea 4)
+ --Answer = 50.2857
+ 
